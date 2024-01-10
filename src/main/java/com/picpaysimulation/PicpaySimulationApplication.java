a411a34237c6c195b8e4ff3d825df75cc8a629fb
Davi Marinho <davimarinho7742@gmail.com>
2024-01-10 08:57:01 -0300

    feat: initial commit

---
+++ src/main/java/com/picpaysimulation/PicpaySimulationApplication.java
@@ -0,0 +1,13 @@
+package com.picpaysimulation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PicpaySimulationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PicpaySimulationApplication.class, args);
+	}
+
+}
