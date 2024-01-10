5c05332bd5420d5b8f684be0c1decd2cd206dedc
Davi Marinho <davimarinho7742@gmail.com>
2024-01-10 08:58:56 -0300

    feat: create UserRepository

---
+++ src/main/java/com/picpaysimulation/repositories/UserRepository.java
@@ -0,0 +1,12 @@
+package com.picpaysimulation.repositories;
+
+import com.picpaysimulation.domain.user.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findUserByDocument(String document);
+    Optional<User> findUserById(Long id);
+}
