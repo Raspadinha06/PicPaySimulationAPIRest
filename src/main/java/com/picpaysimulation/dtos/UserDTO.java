04b0fb899696e4aca720949496964acd45ad1a4c
Davi Marinho <davimarinho7742@gmail.com>
2024-01-15 08:46:05 -0300

    feat: create UserDTO

---
+++ src/main/java/com/picpaysimulation/dtos/UserDTO.java
@@ -0,0 +1,14 @@
+package com.picpaysimulation.dtos;
+
+import com.picpaysimulation.domain.user.UserType;
+
+import java.math.BigDecimal;
+
+public record UserDTO(String firstName,
+                      String lastName,
+                      String document,
+                      String email,
+                      String password,
+                      BigDecimal balance,
+                      UserType userType) {
+}
